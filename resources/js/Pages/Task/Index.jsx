4e97eceec4b8c0ab29bd37a81f93d84b47872079
Roei Boribor <roeiboribor@gmail.com>
2023-04-05 19:58:50 +0800

    minor fix to checbox table

--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -125,7 +125,11 @@ export default function Index({ auth, tasks }) {
                                                         key={id}
                                                         id={`checkbox-${id}`}
                                                         type="checkbox"
-                                                        checked={!is_completed}
+                                                        checked={
+                                                            is_completed
+                                                                ? "True"
+                                                                : ""
+                                                        }
                                                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                                                     />
                                                 </th>
