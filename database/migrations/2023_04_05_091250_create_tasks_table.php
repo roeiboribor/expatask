e044d7ae1c0136343c777beca38d4988cbfe6248
Roei Boribor <roeiboribor@gmail.com>
2023-04-05 17:15:14 +0800

    add columns

--- database/migrations/2023_04_05_091250_create_tasks_table.php
+++ database/migrations/2023_04_05_091250_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('is_completed')->default(0);
             $table->timestamps();
         });
     }
